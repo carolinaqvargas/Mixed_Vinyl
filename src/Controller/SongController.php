72883c98981a1be770d663accecd99d744974ea7
Carolina Vargas <carolinaqvargas@gmail.com>
2023-07-25 17:19:50 +0200

    logger

--- src/Controller/SongController.php
+++ src/Controller/SongController.php
@@ -6,11 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Psr\Log\LoggerInterface;
 
 class SongController extends AbstractController
 {
     #[Route('/api/songs/{id<\d+>}', methods: ['GET'])]  //d+ regex meaning a digit of any lenth
-    public function getSong(int $id): Response
+    public function getSong(int $id, LoggerInterface $logger): Response
     {
         // TODO query the database
         $song = [
@@ -18,6 +19,8 @@ class SongController extends AbstractController
             'name' => 'Waterfalls',
             'url' => 'https://symfonycasts.s3.amazonaws.com/sample.mp3',
         ];
+
+        $logger->info('Returning API response for song {song}', ['song' => $id]);
 
        //return new JsonResponse($song);
        return $this->json($song);
